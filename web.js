30f01c1607625488be5382b4e6a29a3ee78e0e15
Jaspal Singh Saluja <jaspal.saluja@gmail.com>
2013-07-11 01:49:36 +0000

    Read string from index.html in web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+var str = new Buffer(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
